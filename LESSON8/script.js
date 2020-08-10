972ed682ac0f96fa3c11e3be3dfa718dc9ff5754
SergeiSemenov <dp120291ssv@gmail>
2020-08-11 00:17:43 +0300

    hw 8

--- LESSON8/script.js
+++ LESSON8/script.js
@@ -4,9 +4,9 @@ arrNaturalNumber = []
 getNaturalNumberArr(20);
 
 function getNaturalNumberArr(number) {
-    nexStep: for (let i = 2; i <= number; i++) { // Для всех i...
-        for (let j = 2; j < i; j++) { // проверить, делится ли число..
-            if (i % j == 0) continue nexStep; // не подходит, берём следующее
+    nexStep: for (let i = 2; i <= number; i++) {
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) continue nexStep;
 
         }
         arrNaturalNumber.push(i);

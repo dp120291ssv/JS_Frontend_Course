49f120795159dd5401befe74de0a562e43427801
SergeiSemenov <dp120291ssv@gmail>
2020-08-19 12:01:31 +0300

    add 10 HW

--- LESSON10/script.js
+++ LESSON10/script.js
@@ -2,55 +2,67 @@ const addBtn = document.querySelector('#add');
 const dltBtn = document.querySelector('#add');
 const inputField = document.querySelector('#field');
 const items = document.querySelector('#taskList');
-const deleteBtn = document.querySelector('.item-delete')
+const btnDeleteAll = document.querySelector('#del');
 
-addBtn.addEventListener('click', addTask);
-dltBtn.addEventListener('click', deleteItem);
-items.addEventListener('click', onItemClick);
-
-//добавил создание списка с помощью Enter клавиши
-inputField.addEventListener("keypress", (keyPressed) => {
-    const keyEnter = 13;
-    if (keyPressed.which == keyEnter) {
-        addTask();
-    }
-});
-
-function createNewItem(task) {
-    const item = document.createElement('li');
-    item.insertAdjacentHTML('beforeend', `<li><div class="item-block"><span class="item-text">${task}</span><img src="img/delete.png" width=25px class="item-delete"></div></li>`)
-    return item;
-}
+startProccess();
+setEnterKeyHendler();
 
 function addTask() {
     if (inputField.value)
         items.appendChild(createNewItem(inputField.value));
     clearInput();
+    setDeleteHandler();
+    setDeleteAllHandler();
+    setStatusHendler();
+}
+
+function createNewItem(task) {
+    const item = document.createElement('li');
+    item.insertAdjacentHTML('beforeend', `<div class="item-block"><span class="item-text">${task}</span><img src="img/delete.png" width=25px class="item-delete"></div>`)
+    return item;
 }
 
 function clearInput() {
     return inputField.value = "";
 }
 
-function onItemClick(event) {
+function setEnterKeyHendler() {
+    inputField.addEventListener("keypress", (keyPressed) => {
+        const keyEnter = 13;
+        if (keyPressed.which == keyEnter) {
+            addTask();
+        }
+    });
+}
+
+function startProccess() {
+    addBtn.addEventListener('click', addTask);
+}
+
+function setStatusHendler() {
+    items.addEventListener('click', changeStatus);
+}
+
+function setDeleteAllHandler() {
+    btnDeleteAll.addEventListener('click', deleteAllElement);
+}
+
+function setDeleteHandler() {
+    const deleteButtons = document.getElementsByClassName('item-delete');
+    for (let i = 0; i < deleteButtons.length; i++) {
+        deleteButtons[i].addEventListener('click', deleteElement);
+    }
+}
+
+function changeStatus(event) {
     if (event.target.classList.contains('item-block'))
         event.target.classList.toggle('done');
 }
 
-function deleteItem() {
+function deleteElement(e) {
+    e.target.parentNode.parentNode.remove()
+}
 
-    // const itemsList = document.querySelectorAll('#taskList');
-
-    // let tab = [],
-    //     liIndex;
-
-    // for (const i = 0; i < itemsList.length; i++) {
-    //     tab.push(items[i].innerHTML)
-    // }
-
-    // for (const i = 0; i < itemsList.length; i++) {
-    //     itemsList[i].onItemClick = function() {
-    //         liIndex = tab.indexOf(this.innerHTML);
-    //     }
-    // }
+function deleteAllElement() {
+    items.innerHTML = '';
 }

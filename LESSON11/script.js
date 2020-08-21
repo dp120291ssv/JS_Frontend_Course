0862d6e0752d7a8f2a20d0d12cf63eba325b5cf8
SergeiSemenov <dp120291ssv@gmail>
2020-08-21 23:45:17 +0300

    hw 11 fix

--- LESSON11/script.js
+++ LESSON11/script.js
@@ -1,5 +1,4 @@
-function createCalculator(name) {
-    let counts = 0;
+function createCalculator(counts) {
     return {
         sum: function(someNum) {
             counts += someNum;
@@ -17,20 +16,21 @@ function createCalculator(name) {
             console.log(name, counts);
         }
     }
+    return counts;
 }
 
-const calc = createCalculator("результат:");
+const calc = createCalculator(10);
 
 /**Для примера*/
 calc.sum(10);
-calc.sum(5);
-calc.mult(10);
-calc.mult(2);
-calc.sum(33);
-calc.div(3);
-calc.set();
-calc.sum(200);
-calc.mult(1.5);
-calc.div(60);
-calc.set();
+// calc.sum(5);
+// calc.mult(10);
+// calc.mult(2);
+// calc.sum(33);
+// calc.div(3);
+// calc.set();
+// calc.sum(200);
+// calc.mult(1.5);
+// calc.div(60);
+// calc.set();
 calc.log();

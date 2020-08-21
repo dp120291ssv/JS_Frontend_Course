b71e04631b3be69cc3a00d3b38dbef70e47ac67a
SergeiSemenov <dp120291ssv@gmail>
2020-08-21 23:46:08 +0300

    hw 11 fix

--- LESSON11/script.js
+++ LESSON11/script.js
@@ -19,18 +19,17 @@ function createCalculator(counts) {
     return counts;
 }
 
-const calc = createCalculator(10);
+const calc = createCalculator(33);
 
 /**Для примера*/
 calc.sum(10);
-// calc.sum(5);
-// calc.mult(10);
-// calc.mult(2);
-// calc.sum(33);
-// calc.div(3);
-// calc.set();
-// calc.sum(200);
-// calc.mult(1.5);
-// calc.div(60);
-// calc.set();
+calc.sum(5);
+calc.mult(10);
+calc.mult(2);
+calc.sum(33);
+calc.div(3);
+calc.set();
+calc.sum(200);
+calc.mult(1.5);
+calc.div(60);
 calc.log();

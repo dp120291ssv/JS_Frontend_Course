06596f42e47a2437d0bd3e4b86899efd1d0c8988
SergeiSemenov <dp120291ssv@gmail>
2020-09-01 13:58:21 +0300

    HW - 12

---
+++ LESSON11/script2.js
@@ -0,0 +1,37 @@
+function createCalculator(defaultNum) {
+    let expression = defaultNum;
+    return {
+        sum: (someNum) => {
+            expression += ' + ' + someNum
+            return defaultNum += someNum;
+        },
+        mult: (someNum) => {
+            expression += ' * ' + someNum
+            return defaultNum *= someNum;
+        },
+        sub: (someNum) => {
+            expression += ' - ' + someNum
+            return defaultNum -= someNum;
+        },
+        div: (someNum) => {
+            expression += ' / ' + someNum
+            return defaultNum /= someNum;
+        },
+        log: () => {
+            return console.log(name, defaultNum);
+        },
+        getResult: () => {
+            return expression;
+        }
+    }
+}
+
+const calc = createCalculator(10);
+
+/**Для примера*/
+calc.sum(5); // 15
+calc.mult(10); // 150
+calc.sub(40); // 110
+calc.div(10); // 11
+calc.log();
+console.log(calc.getResult());
---
+++ LESSON12/script.js
@@ -0,0 +1,36 @@
+function СreateCalculator(defaultNum) {
+    let expression = defaultNum;
+    this.sum = (someNum) => {
+            expression += ' + ' + someNum
+            return defaultNum += someNum;
+        },
+        this.mult = (someNum) => {
+            expression += ' * ' + someNum
+            return defaultNum *= someNum;
+        },
+        this.sub = (someNum) => {
+            expression += ' - ' + someNum
+            return defaultNum -= someNum;
+        },
+        this.div = (someNum) => {
+            expression += ' / ' + someNum
+            return defaultNum /= someNum;
+        },
+        this.log = () => {
+            return console.log("", defaultNum);
+        },
+        this.getResult = () => {
+            return expression;
+        }
+}
+
+
+const calc = new СreateCalculator(10);
+
+/**Для примера*/
+calc.sum(5); // 15
+calc.mult(10); // 150
+calc.sub(40); // 110
+calc.div(10); // 11
+calc.log();
+console.log(calc.getResult());

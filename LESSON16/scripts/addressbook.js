1e2e72e6d1e5145f5274945ab07dffc38176e0af
dn120291ssv1 <Smnv0969867628>
2020-09-23 13:52:11 +0300

    HW15

--- LESSON16/scripts/addressbook.js
+++ LESSON16/scripts/addressbook.js
@@ -1,88 +1,74 @@
-window.addEventListener('load', function() {
-	// buttons
-	const quickAddBtn = document.getElementById('QuickAdd');
-	const quickAddFormDiv = document.querySelector('.quickaddForm')
-	const cancelBtn = document.getElementById('Cancel');
-	const addBtn = document.getElementById('Add');
-	// form fields
-	const fullname = document.getElementById('fullname');
-	const phone = document.getElementById('phone');
-	const address = document.getElementById('address');
-	const city = document.getElementById('city');
-	const email = document.getElementById('email');
-	// some el
-	const addBookDiv = document.querySelector('.addbook');
+window.addEventListener("load", function () {
 
-	quickAddBtn.addEventListener("click", function(){
-		quickAddFormDiv.style.display = "block";
-	});
+  const quickAddBtn = document.getElementById("QuickAdd");
+  const quickAddFormDiv = document.querySelector(".quickaddForm");
+  const cancelBtn = document.getElementById("Cancel");
+  const addBtn = document.getElementById("Add");
+  const fullname = document.getElementById("fullname");
+  const phone = document.getElementById("phone");
+  const email = document.getElementById("email");
+  const addBookDiv = document.querySelector(".addbook");
 
-	cancelBtn.addEventListener("click", function(){
-		quickAddFormDiv.style.display = "none";
-	});
+  quickAddBtn.addEventListener("click", function () {
+    quickAddFormDiv.style.display = "block";
+  });
 
-	addBtn.addEventListener("click", addToBook);
+  cancelBtn.addEventListener("click", function () {
+    quickAddFormDiv.style.display = "none";
+  });
 
-	addBookDiv.addEventListener("click", removeEntry);
+  addBtn.addEventListener("click", addToBook);
 
-	// Storage Array
-	var addressBook = [];
+  addBookDiv.addEventListener("click", removeEntry);
 
-	function JsonStructure(fullname,phone,email){
-		this.fullname = fullname;
-		this.phone = phone;
-		this.email = email;
-	}
+  var addressBook = [];
 
-	function addToBook(){
-		var isNull = fullname.value!='' && phone.value!='' && email.value!='';
-		if(isNull){
-			// format the input into a valid JSON structure
-			const obj = new JsonStructure(fullname.value,phone.value,email.value);
-			addressBook.push(obj);
-			localStorage['addbook'] = JSON.stringify(addressBook);
-			quickAddFormDiv.style.display = "none";
-			clearForm();
-			showAddressBook();
-		}
-	}
+  function JsonStructure(fullname, phone, email) {
+    this.fullname = fullname;
+    this.phone = phone;
+    this.email = email;
+  }
 
-	function removeEntry(e){
-		// Remove an entry from the addressbook
-		if(e.target.classList.contains('delbutton')){
-			var remID = e.target.getAttribute('data-id');
-			addressBook.splice(remID,1);
-			localStorage['addbook'] = JSON.stringify(addressBook);
-			showAddressBook();
-		}
-	}
+  function addToBook() {
+    const isElNull = fullname.value != "" && phone.value != "" && email.value != "";
+    if (isElNull) {
+      const addressBookObject = new JsonStructure(fullname.value, phone.value, email.value);
+      addressBook.push(addressBookObject);
+      localStorage["addbook"] = JSON.stringify(addressBook);
+      quickAddFormDiv.style.display = "none";
+      clearForm();
+      showAddressBook();
+    }
+  }
 
-	function clearForm(){
-		var formFields = document.querySelectorAll('.formFields');
-		for(var i in formFields){
-			formFields[i].value = '';
-		}
-	}
+  function removeEntry(e) {
+    if (e.target.classList.contains("delbutton")) {
+      let remID = e.target.getAttribute("data-id");
+      addressBook.splice(remID, 1);
+      localStorage["addbook"] = JSON.stringify(addressBook);
+      showAddressBook();
+    }
+  }
 
-	function showAddressBook(){
-		if(localStorage['addbook'] === undefined){
-			localStorage['addbook'] = '';
-		} else {
-			addressBook = JSON.parse(localStorage['addbook']);
-			// Loop over the array addressBook and insert into the page
-			addBookDiv.innerHTML = '';
-			for(var n in addressBook){
-				var str = '<div class="entry">';
-					str += '<div class="name"><p>' + addressBook[n].fullname + '</p></div>';
-					str += '<div class="email"><p>' + addressBook[n].email + '</p></div>';
-					str += '<div class="phone"><p>' + addressBook[n].phone + '</p></div>';
-					str += '<div class="del"><a href="#" class="delbutton" data-id="' + n + '">Delete</a></div>';
-					str += '</div>';
-				addBookDiv.innerHTML += str;
-			}
-		}
-	}
+  function clearForm() {
+    let formFields = document.querySelectorAll(".formFields");
+    for (let i in formFields) {
+      formFields[i].value = "";
+    }
+  }
 
-	showAddressBook();
-
-})
+  function showAddressBook() {
+    addressBook = JSON.parse(localStorage["addbook"]);
+    addBookDiv.innerHTML = "";
+    for (let n in addressBook) {
+      let str = '<div class="entry">';
+      str += '<div class="name"><p>' + addressBook[n].fullname + "</p></div>";
+      str += '<div class="email"><p>' + addressBook[n].email + "</p></div>";
+      str += '<div class="phone"><p>' + addressBook[n].phone + "</p></div>";
+      str += '<div class="del"><a href="#" class="delbutton" data-id="' + n + '">Delete</a></div>';
+      str += "</div>";
+      addBookDiv.innerHTML += str;
+    }
+  }
+  showAddressBook();
+});

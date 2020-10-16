2f4edea5d1a8603f909b84e205694e7d24820bf4
dn120291ssv1 <Smnv0969867628>
2020-10-16 12:07:36 +0300

    HW20

--- LESSON20/scripts/main.js
+++ LESSON20/scripts/main.js
@@ -1,166 +1,129 @@
-$(function () {
-    const URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/stickers';
+const URL = 'https://5dd3d5ba8b5e080014dc4bfa.mockapi.io/stickers';
 
-    let notes = [];
-    const noteTemplate = $('#noteTemplate').html();
-    const $field = $('#field');
-    const dialog = $('#dialog-form').dialog({
-        autoOpen: false,
-        height: 400,
-        width: 350,
-        modal: true,
-        buttons: {
-            'Create Note': addNote,
-            Cancel: function () {
-                dialog.dialog('close');
-            },
+const DELETE_NOTE_CLASS = 'delete-note';
+const EDIT_NOTE_CONTROL_CLASS = 'edit-note-control';
+
+const noteTemplate = document.getElementById('noteTemplate').innerHTML;
+const fieldElement = document.getElementById('field');
+
+let notesList = [];
+
+document
+    .getElementById('addNoteBtn')
+    .addEventListener('click', onAddNoteBtnClick);
+
+fieldElement.addEventListener('click', onFieldElementClick);
+fieldElement.addEventListener('focusout', onFieldElementFocusout);
+init();
+
+function onAddNoteBtnClick() {
+    createNote();
+}
+
+function onClearAllBtnClick() {
+    clearAll();
+}
+
+function onFieldElementClick(e) {
+    switch (true) {
+        case e.target.classList.contains(DELETE_NOTE_CLASS):
+            deleteNote(e.target.parentElement.dataset.noteIndex);
+            break;
+    }
+}
+
+function onFieldElementFocusout(e) {
+    const element = e.target;
+
+    switch (true) {
+        case e.target.classList.contains(EDIT_NOTE_CONTROL_CLASS):
+            updateNote(
+                element.parentElement.dataset.noteIndex,
+                element.name,
+                element.value
+            );
+            break;
+    }
+}
+
+function init() {
+    getList();
+}
+
+function getList() {
+    fetch(URL)
+        .then((res) => res.json())
+        .then(setData)
+        .then(renderList);
+}
+
+function setData(data) {
+    return (notesList = data);
+}
+
+function getNoteElement(id) {
+    return fieldElement.querySelector(`[data-note-index="${id}"]`);
+}
+
+function createNote() {
+    const note = {
+        description: '',
+    };
+
+    fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
         },
-        close: function () {
-            form[0].reset();
+        body: JSON.stringify(note),
+    })
+        .then((res) => res.json())
+        .then((note) => {
+            notesList.push(note);
+            renderNote(note);
+        });
+}
+
+function updateNote(id, name, value) {
+    const note = notesList.find((el) => el.id == id);
+
+    note[name] = value;
+
+    fetch(`${URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
         },
+        body: JSON.stringify(note),
     });
-    const form = dialog.find('form').on('submit', onFormSubmit);
+}
 
-    $('#addNoteBtn').on('click', () => dialog.dialog('open'));
-    $field.on('click', '.delete-note', onDeleteNoteClick);
-    $field.on('focusout', '.edit-note-control', onNoteFocusout);
+function deleteNote(id) {
+    notesList = notesList.filter((el) => el.id != id);
 
-    init();
+    deleteNoteElement(id);
 
-    function init() {
-        getNotes();
-    }
+    fetch(`${URL}/${id}`, {
+        method: 'DELETE',
+    });
+}
 
-    function getNotes() {
-        fetch(URL)
-            .then((res) => res.json())
-            .then(setNotes)
-            .then(renderNotes);
-    }
+function deleteNoteElement(id) {
+    const element = getNoteElement(id);
 
-    function setNotes(data) {
-        return (notes = data);
-    }
+    element && element.remove();
+}
 
-    function renderNotes(notes) {
-        notes.forEach(renderNote);
-    }
+function renderList(notesList) {
+    notesList.forEach(renderNote);
+}
 
-    function onFormSubmit(event) {
-        event.preventDefault();
-        addNote();
-    }
+function renderNote(note) {
+    fieldElement.insertAdjacentHTML('beforeEnd', getNoteHtml(note));
+}
 
-    function onDeleteNoteClick() {
-        const $note = $(this).closest('.note');
-        const noteId = $note.data('id');
-
-        deleteNoteElement(noteId);
-
-        deleteNote(noteId);
-    }
-
-    function onNoteFocusout() {
-        const $el = $(this);
-        const $note = $el.closest('.note');
-        const noteId = $note.data('id');
-
-        changeNote(noteId, { description: $el.val() });
-    }
-
-    function deleteNoteElement(id) {
-        console.log('deleteNoteElement', id, $(`.note[data-id="${id}"]`));
-        const $note = $(`.note[data-id="${id}"]`);
-        $note.remove();
-    }
-
-    function addNote() {
-        const note = {
-            x: 50,
-            y: 50,
-            width: 100,
-            height: 250,
-        };
-
-        form.serializeArray().forEach((v) => (note[v.name] = v.value));
-        createNote(note);
-
-        dialog.dialog('close');
-    }
-
-    function deleteNote(id) {
-        notes = notes.filter((note) => note.id !== id);
-        deleteNote(id);
-    }
-
-    function renderNote(note) {
-        const $note = $(
-            noteTemplate
-                .replace('{{id}}', note.id)
-                .replace('{{title}}', '#' + note.id)
-                .replace('{{description}}', note.description)
-        ).css({
-            left: note.x,
-            top: note.y,
-            width: note.width,
-            height: note.height,
-        });
-        $field.append($note);
-
-        $note
-            .draggable({
-                handle: 'h5',
-                stop: function (e, ui) {
-                    changeNote(note.id, {
-                        x: ui.position.left,
-                        y: ui.position.top,
-                    });
-                },
-            })
-            .resizable({
-                stop: function (e, ui) {
-                    changeNote(note.id, ui.size);
-                },
-            });
-    }
-
-    function changeNote(id, changes) {
-        const note = { ...notes.find((item) => item.id == id), ...changes };
-        notes = notes.map((item) => (item.id != id ? item : note));
-
-        console.log(id, note, changes);
-
-        updateNote(note);
-    }
-
-    function createNote(note) {
-        return fetch(URL, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(note),
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                notes.push(data);
-                renderNote(data);
-            });
-    }
-
-    function updateNote(note) {
-        return fetch(URL + '/' + note.id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(note),
-        });
-    }
-
-    function deleteNote(id) {
-        return fetch(URL + '/' + id, {
-            method: 'DELETE',
-        });
-    }
-});
+function getNoteHtml(note) {
+    return noteTemplate
+        .replace('{{id}}', note.id)
+        .replace('{{description}}', note.description);
+}

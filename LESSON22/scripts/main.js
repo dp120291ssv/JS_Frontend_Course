f07be01361d503f63c8f7eeb72ab91a9e37589fe
dn120291ssv1 <Smnv0969867628>
2020-10-20 14:32:25 +0300

    HW22

---
+++ LESSON22/scripts/main.js
@@ -0,0 +1,35 @@
+$('#photos').justifiedGallery({
+    rowHeight : 70,
+    margins : 3
+});
+
+$(() => {
+
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?';
+const photosEl = document.querySelector('#photos');
+const photoItemTemplate = document.querySelector('#photoItemTemplate').innerHTML;
+
+init();
+
+function init() {
+    getPhotos();
+}
+
+function getPhotos() {
+  return fetch(PHOTOS_URL)
+      .then((resp) => resp.json())
+      .then(renderPhotos);
+}
+
+function renderPhotos(data) {
+    photosEl.innerHTML = data
+        .map((photo) => setPhotosParamsInHTML(photo))
+        .join('\n');
+}
+
+function setPhotosParamsInHTML(photo) {
+    return photoItemTemplate
+        .replace('{{url}}', photo.url)
+        .replace('{{thumbnailUrl}}', photo.thumbnailUrl)
+    }
+});

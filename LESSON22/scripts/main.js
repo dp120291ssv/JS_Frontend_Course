ee2c2992cf655283b336cabeafb631142cbc9fb4
dn120291ssv1 <Smnv0969867628>
2020-10-22 21:59:47 +0300

    add changes

--- LESSON22/scripts/main.js
+++ LESSON22/scripts/main.js
@@ -1,35 +1,33 @@
-$('#photos').justifiedGallery({
-    rowHeight : 70,
-    margins : 3
-});
-
 $(() => {
+  const PHOTOS_URL = "https://jsonplaceholder.typicode.com/photos?";
+  const photosEl = document.querySelector("#photos");
+  const photoItemTemplate = document.querySelector("#photoItemTemplate").innerHTML;
 
-const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos?';
-const photosEl = document.querySelector('#photos');
-const photoItemTemplate = document.querySelector('#photoItemTemplate').innerHTML;
+  init();
 
-init();
-
-function init() {
+  function init() {
     getPhotos();
-}
+  }
 
-function getPhotos() {
-  return fetch(PHOTOS_URL)
+  function getPhotos() {
+    return fetch(PHOTOS_URL)
       .then((resp) => resp.json())
       .then(renderPhotos);
-}
+  }
 
-function renderPhotos(data) {
+  function renderPhotos(data) {
     photosEl.innerHTML = data
-        .map((photo) => setPhotosParamsInHTML(photo))
-        .join('\n');
-}
+      .map((photo) => setPhotosParamsInHTML(photo))
+      .join("\n");
+      $("#photos").justifiedGallery({
+        rowHeight: 70,
+        margins: 3,
+      });
+  }
 
-function setPhotosParamsInHTML(photo) {
+  function setPhotosParamsInHTML(photo) {
     return photoItemTemplate
-        .replace('{{url}}', photo.url)
-        .replace('{{thumbnailUrl}}', photo.thumbnailUrl)
-    }
+      .replace("{{url}}", photo.url)
+      .replace("{{thumbnailUrl}}", photo.thumbnailUrl);
+  }
 });
